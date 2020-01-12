b72586b1d485ebc675c20fd1fa6f2c33e0c1c5a4
Hari Govind <grhari1998@gmail.com>
2020-01-12 12:11:24 -0500

    finished the easy bits

--- BarcodeScanner/app/src/main/java/com/example/barcodescanner/ResultActivity.java
+++ BarcodeScanner/app/src/main/java/com/example/barcodescanner/ResultActivity.java
@@ -2,13 +2,11 @@ package com.example.barcodescanner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.widget.TextView;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 import java.io.IOException;
 
@@ -19,7 +17,10 @@ import okhttp3.Request;
 import okhttp3.Response;
 
 public class ResultActivity extends AppCompatActivity {
-    private TextView mTextViewResult;
+    private TextView styleText;
+    private TextView materialsText;
+    private TextView manutext;
+    private TextView estText;
     private JSONObject trial;
 //    int id = getIntent().getIntExtra("bid", 0 );
 
@@ -34,7 +35,10 @@ public class ResultActivity extends AppCompatActivity {
     }
 
     private void request(int barcode){
-        mTextViewResult = findViewById(R.id.materials);
+        styleText     = findViewById(R.id.styleName);
+        materialsText = findViewById(R.id.materials);
+        manutext = findViewById(R.id.manu);
+        estText = findViewById(R.id.est);
         String url = "https://ecostep.herokuapp.com/items/" + barcode;
         OkHttpClient client = new OkHttpClient();
 
@@ -63,7 +67,8 @@ public class ResultActivity extends AppCompatActivity {
                         @Override
                         public void run() {
                             try {
-                                mTextViewResult.setText(trial.getString("String"));
+                                styleText.setText(trial.getString("style"));
+                                manutext.setText(trial.getString("madeIn"));
                             } catch (JSONException e) {
                                 e.printStackTrace();
                             }
